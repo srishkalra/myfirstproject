bc82e762a3da91d73672d836107f8169e08207d7
srishkalra <69515294+srishkalra@users.noreply.github.com>
2020-08-16 21:30:17 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+# Importing flask module in the project
+# An object of Flask class is our WSGI application
+from flask import Flask,jsonify
+import logging
+import git
+import os
+port = os.environ.get('PORT')
+version = os.environ.get('VERSION')
+# Flask constructor takes the name of current module (__name__) as argument.
+app = Flask(__name__)
+#Initializing object of logging.logger
+applog = logging.getLogger(__name__)
+git_repo = git.Repo(search_parent_directories=True)
+sha_id = git_repo.head.object.hexsha
+print(type(applog))
+#Setting the log level as INFO
+applog.setLevel(logging.INFO)
+
+#print(applog.getEffectiveLevel())
+logging.basicConfig(filename="myapp.log",format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+service = [
+    {
+        'servic': f"{app.name}",
+        'version': "{}".format(VERSION),
+         'git sha': "{}".format(sha_id)
+    },
+    {
+        'port': "{}".format(port),
+        'loglevel': f"{logging.getLevelName(applog.level)}"
+    }
+]
+@app.route('/',methods=['GET'])
+def hello_world():
+    app.logger.info('Displaying hello')
+    return 'Hello, World!:'
+
+@app.route('/info', methods=['GET'])
+def get_myapp_info():
+    app.logger.info('Displaying the info logs')
+    return jsonify({'service': service},200)
+
+app.run(host='0.0.0.0', port="{}".format(port))
